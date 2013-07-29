bada9fbafeb6f2a5794d5feb050b6892ef4d3bfc
Avishek Kumar <avishek.kumar@asu.edu>
2013-07-28 20:47:34 -0700

    Making connection is working

--- bilayer_stats/main.cpp
+++ bilayer_stats/main.cpp
@@ -4,9 +4,11 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 #include "vertex.h"
 #include "graph.h"
+
 
 //global variables 
 Graph bilayer; 
@@ -36,9 +38,7 @@ void read_xyz(char *file, Graph &bilayer)
       printf("%s %.15f %.15f %.15f\n",atom,x,y,z);
       if (atom[0] == 'O')
 	{
-	  Vertex* pos = new Vertex(8,x,y,z); 
-	  bilayer.vertices.push_back(pos); 
-	  std::cout<< bilayer.vertices.size() << std::endl; 
+	  continue; 
 	}
       else
 	{
@@ -58,17 +58,67 @@ void read_xyz(char *file, Graph &bilayer)
 
 }//read_xyz()
 
-void connectatoms(Graph &bilayer,int atomno1, int atomno2)
+
+/*
+  Connect atoms within a certain distance 
+ */
+void connectatoms(Graph &bilayer,float dist,int Debug=0)
 {
+  
   for(unsigned int i = 0; i < bilayer.vertices.size(); i++)
     {
-      std::cout << "Atomic Number: " << bilayer.vertices[i]->atomno << std::endl; 
-      if(bilayer.vertices[i]->atomno == 8)
+      for(unsigned int j = i+1; j < bilayer.vertices.size(); j++)
 	{
-	  std::cout << bilayer.vertices[i]->x << " " << bilayer.vertices[i]->y << " " << bilayer.vertices[i]->z << std::endl; 
-	}
-    }
+	  float x_ij = bilayer.vertices[i]->x - bilayer.vertices[j]->x; 
+	  float y_ij = bilayer.vertices[i]->y - bilayer.vertices[j]->y;
+	  float z_ij = bilayer.vertices[i]->z - bilayer.vertices[j]->z;
+	  float r = sqrt( x_ij*x_ij + y_ij*y_ij + z_ij*z_ij ); 
+	  
+	  if(Debug)
+	    {
+	      std::cout << "Vertex: " << i << " " << bilayer.vertices[i]->x << " " << bilayer.vertices[i]->y << " " << bilayer.vertices[i]->z << std::endl; 
+	      std::cout << "Vertex: " << j << " " << bilayer.vertices[j]->x << " " << bilayer.vertices[j]->y << " " << bilayer.vertices[j]->z << std::endl; 
+	      std::cout << "x_ij " << x_ij << std::endl; 
+	      std::cout << "y_ij " << y_ij << std::endl; 
+	      std::cout << "z_ij " << z_ij << std::endl; 
+	      std::cout << "r: " << r << std::endl; 
+	  
+		
+	    }
+	  if(r > 0.0 && r < dist)
+	    {
+	      std::cout << "Match" << std::endl; 
+	      bilayer.vertices[i]->AddEdge(bilayer.vertices[j]); 
+	    }
+
+	}//j loop 
+    }//i loop
 }//connectatoms
+
+void MakeHoney(Graph& bilayer, char *nfile)
+{
+  FILE *outFile; 
+  outFile = fopen(nfile , "w");
+  fprintf(outFile, "Graphics[{Black");
+
+
+  for(unsigned int j =0; j < bilayer.vertices.size(); j++)
+    {
+      for(unsigned int i =0; i < bilayer.vertices[j]->edges.size(); i++)
+        {
+          
+	  fprintf(outFile, ", \nLine[{{%f,%f},{%f,%f}}]", bilayer.vertices[j]->x,
+		  bilayer.vertices[j]->y,
+		  bilayer.vertices[j]->edges[i]->x,
+		  bilayer.vertices[j]->edges[i]->y);
+	  
+        }
+    }
+
+  fprintf(outFile, "\n}]");
+  fclose(outFile);
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -81,7 +131,8 @@ int main(int argc, char *argv[])
 
   
   read_xyz(argv[1],bilayer);
-  connectatoms(bilayer,8,14);
+  connectatoms(bilayer,2.1);
+  MakeHoney(bilayer,"honeycomb.m"); 
 
   return 0; 
 }//main()
