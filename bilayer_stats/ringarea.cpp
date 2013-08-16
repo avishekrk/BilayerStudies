d8c6853b289a1652ac17242777b19f5fbfe05b52
Avishek Kumar <avishek.kumar@asu.edu>
2013-08-16 03:37:24 -0700

    Working on Ring Sorting

--- bilayer_stats/ringarea.cpp
+++ bilayer_stats/ringarea.cpp
@@ -14,11 +14,65 @@ void Hello()
   std::cout << "Hello World" << std::endl; 
 }//Hello
 
+
+/*
+  sortRing, sorts ring vertices in the order that they are connected
+  @param, ring, n vector of Vertex*
+  @return, sring, sorted ring
+ */
+std::vector<Vertex*> ringSort(std::vector<Vertex*> &ring,bool Debug=false)
+{
+  std::vector<Vertex*> cycle = ring; 
+  std::vector<Vertex*> sorted;
+
+
+  if(Debug)
+    {
+      std::cout << "Pre-Sorted Ring Coordinates" << std::endl; 
+      for(unsigned int i = 0; i < cycle.size(); i++)
+	std::cout << cycle[i]->x << " " << cycle[i]->y << std::endl; 
+      
+      for(unsigned int i = 0; i < cycle[0]->edges.size(); i++)
+	{
+	  std::cout << cycle[0]->edges[i]->index << ": " << cycle[0]->edges[i]->x << " " << cycle[0]->edges[i]->y << std::endl; 
+	}
+
+    }//end Debug 
+  
+
+  sorted.push_back(cycle[0]); 
+  for(unsigned int i = 0; i < cycle[0]->edges.size(); i++)
+    {
+      bool Match=false;
+      for(unsigned int j = 0; j < cycle.size(); j++)
+	if (cycle[0]->edges[i] == cycle[j])
+	  {
+	    std::cout << "Found a Match" << std::endl;
+	    sorted.push_back(cycle[j]); 
+	    Match = true;
+	    break; 
+	  }
+      if(Match)
+	break; 
+    }
+  
+  std::cout<< "Sorted List" << std::endl; 
+  for(unsigned int i = 0; i < sorted.size(); i++)
+    std::cout << sorted[i]->x << " " << sorted[i]->y << std::endl; 
+
+
+  return cycle; 
+
+}//sortRing()
+
 /*
   ringArea, calculated the area of of convex polygon 
  */
-float ringArea(std::vector<Vertex*>  &ring)
+float ringArea(std::vector<Vertex*>  &ring_unsorted)
 {
+
+  std::vector <Vertex*> ring = ringSort(ring_unsorted,true); 
+
   unsigned int n = ring.size() + 1; 
   float *x = new float [n];
   float *y = new float [n]; 
